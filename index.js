c9d78dbac5233024c1146d76fa58aba411e767b1
Ecolipsy <ecolipsy@outlook.com>
2022-08-07 17:47:37 +0200

    Change a bunch of stuff and add command handler

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Variables and stuff
+//Libraries and stuff
 const eris = require("eris");
 const fs = require("fs");
 const utils = require("./utils");
@@ -11,11 +11,89 @@ const doorId = "1005242907487391893";
 const embedColor = 0xfef058;
 const memberAvatars = {}
 
+//Essentials variables
+const commands = [];
+
+class Command{
+    constructor(name, description, run){
+        this.name = name;
+        this.description = description;
+        this.run = run;
+    }
+}
+
+commands.push({name: "ping", description: "Shows bot latency", async run(msg){
+    const ws = client.shards.get(0).ws;
+    const ping = await utils.getPing(ws);
+    console.log(client.shards.get(0));
+    msg.channel.createMessage(`Pong (${ping} ms)`);
+}});
+
+commands.push({name: "localtime", description: "Shows the local time in my server location.", run(msg){
+    msg.channel.createMessage(`The local time over here is: ${new Date().toLocaleTimeString()}`)
+}});
+
+commands.push({name: "bee", description: "Sends an image of a bee.", run(msg){
+    const beeFolder = fs.readdirSync("./bees");
+    const beeImg = beeFolder[Math.floor(Math.random()*beeFolder.length)];
+    const fileName = beeImg;
+    const fileBuffer = fs.readFileSync("./bees/" + fileName);
+    msg.channel.createMessage("Test", {name: fileName, file: fileBuffer});
+}});
+
+commands.push({name: "verify", description: "Use this command to verify.", run(msg){
+    if(msg.guildID !== guildId) return;
+    var server = client.guilds.get(guildId);
+    var member = server.members.get(msg.author.id);
+    member.addRole("1005166680013557831").then(async () => {
+        (await member.user.getDMChannel()).createMessage("You have been verified.");
+    });
+    msg.delete();
+}});
+
+commands.push({name: "beemovie", description: "Sends a random line from the beemovie.", run(msg){
+    const bMovie = fs.readFileSync("beemovie.txt").toString().split("\n");
+    const line = bMovie[Math.floor(Math.random()*bMovie.length)];
+    msg.channel.createMessage(line);
+}});
+
+commands.push({name: "kick", description: "Kicks a member from the current server.", run(msg){
+    const member = msg.member;
+    if(!member || !member.username) return;
+    if(!member.permissions.has("kickMembers")) return msg.channel.createMessage("No.");
+    var args = msg.content.split(" ");
+    args.shift();
+    const toKick = client.guilds.get(msg.guildID).members.get(args[0].replace(/[<@!>]/g, ""));
+    if(!toKick) return msg.channel.createMessage("I didn't find that bee");
+    if(toKick.id === client.user.id) return msg.channel.createMessage("I'd rather not tbh");
+    args.shift();
+    const reason = args.join(" ");
+    toKick.kick(reason);
+}});
+
+commands.push({name: "help", description: "Show a list of commands and what they do.", run(msg){
+    var embed = {
+        title: "Help",
+        description: "Here is a list of commands and what they do:",
+        color: embedColor,
+        thumbnail: {url: msg.author.avatarURL},
+        fields: []
+    }
+    commands.forEach(cmd => {
+        embed.fields.push({name: cmd.name, value: cmd.description});
+    });
+    msg.channel.createMessage({embed}).catch(console.log);
+}});
+
 //Let's hope eris fixed their ready event
 client.on("ready", async() => {
     console.log("Ready!");
     client.guilds.get(guildId).members.forEach(async member => {
         memberAvatars[member.id] = {buffer: await utils.getAvatarBuffer(member.avatarURL), url: member.avatarURL};
+        const userinfoglobal = JSON.parse(fs.readFileSync("userinfo.json".toString()));
+        const userinfo = {messages: 0}
+        userinfoglobal[member.id] = userinfo;
+        fs.writeFileSync("userinfo.json", JSON.stringify(userinfoglobal));
     });
 });
 
@@ -25,31 +103,15 @@ client.on("messageCreate", async (msg) => {
     const isCommand = msg.content.toLowerCase().startsWith(prefix);
     if(!isCommand) return;
     const cmdName = msg.content.split(" ")[0].replace(prefix, "");
-    if(cmdName === "ping"){
-        const ws = client.shards.get(0).ws;
-        const ping = await utils.getPing(ws);
-        console.log(client.shards.get(0));
-        msg.channel.createMessage(`Pong (${ping} ms)`);
-    } else if(cmdName === "localtime"){
-        msg.channel.createMessage(`The local time over here is: ${new Date().toLocaleTimeString()}`)
-    } else if(cmdName === "bee"){
-        const beeFolder = fs.readdirSync("./bees");
-        const beeImg = beeFolder[Math.floor(Math.random()*beeFolder.length)];
-        const fileName = beeImg;
-        const fileBuffer = fs.readFileSync("./bees/" + fileName);
-        msg.channel.createMessage("Test", {name: fileName, file: fileBuffer});
-    } else if(cmdName === "embedtest"){
-        msg.channel.createMessage({embed: {title: "Test", description: "Can you see this?", color: embedColor}});
-    } else if(cmdName === "verify"){
-        if(msg.guildID !== guildId) return;
-        var server = client.guilds.get(guildId);
-        var member = server.members.get(msg.author.id);
-        member.addRole("1005166680013557831");
-        msg.delete();
-    } else if(cmdName === "beemovie"){
-        const bMovie = fs.readFileSync("beemovie.txt").toString().split("\n");
-        const line = bMovie[Math.floor(Math.random()*bMovie.length)];
-        msg.channel.createMessage(line);
+    var found = false;
+    commands.forEach(cmd => {
+        if(cmd.name === cmdName){
+            found = true;
+            cmd.run(msg);
+        }
+    });
+    if(!found){
+        msg.channel.createMessage("Command was not found.");
     }
 });
 
@@ -123,7 +185,6 @@ client.on("messageUpdate", (newM, oldM) => {
 client.on("guildMemberAdd", async (guild, member) => {
     if(guild.id !== guildId) return;
     memberAvatars[member.id] = {url: member.avatarURL, buffer: await utils.getAvatarBuffer(member.avatarURL)}
-    member.addRole("1005166680013557831")
     const channel = client.getChannel(doorId);
     const embed = {
         title: "New Bee",
